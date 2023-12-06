0b111781abfb9c2492d0e0cd655093255ba7da48
zhanghaotian <5048429@qq.com>
2023-12-06 17:27:03 +0800

    Resolve merge conflict in test/test.py

--- test/test.py
+++ test/test.py
@@ -5,3 +5,10 @@ sys.path.append(".")
 
 from src.knapsack import knapsack_01
 
+def test_knapsack_01():
+    values = [60, 100, 120]
+    weights = [10, 20, 30]
+    capacity = 50
+    max_value, selected_items = knapsack_01(values, weights, capacity)
+    assert max_value == 220
+    assert selected_items == [1, 2]
