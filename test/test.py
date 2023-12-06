0164c6db0ee5083d9ea4906472ded595e739c359
zhanghaotian <5048429@qq.com>
2023-12-06 17:20:22 +0800

    testdata

--- test/test.py
+++ test/test.py
@@ -4,10 +4,26 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 from src.knapsack import knapsack_01
 
-values = [60, 100, 120]
-weights = [10, 20, 30]
-capacity = 50
-max_value, selected_items = knapsack_01(values, weights, capacity)
+# Test Case 1
+values1 = [60, 100, 120]
+weights1 = [10, 20, 30]
+capacity1 = 50
+max_value1, selected_items1 = knapsack_01(values1, weights1, capacity1)
+assert max_value1 == 220
+assert selected_items1 == [1, 2]    
 
-assert max_value == 220, "The max_value is wrong"
-assert selected_items == [1, 2], "The selected_items are wrong"
+# Test Case 2
+values2 = [30, 40, 50, 10, 20]
+weights2 = [5, 10, 15, 2, 3]
+capacity2 = 10
+max_value2, selected_items2 = knapsack_01(values2, weights2, capacity2)
+assert max_value2 == 90
+assert selected_items2 == [0, 3, 4]
+
+# Test Case 3
+values3 = [10, 20, 30]
+weights3 = [1, 1, 1]
+capacity3 = 2
+max_value3, selected_items3 = knapsack_01(values3, weights3, capacity3)
+assert max_value3 == 50
+assert selected_items3 == [1, 2]
