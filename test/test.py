3bdf7c72fc28448d524b58f6dfd55dadf944a224
mq <642733045@qq.com>
2023-12-06 16:57:46 +0800

    finish using pytest

--- test/test.py
+++ test/test.py
@@ -1,9 +1,14 @@
+# test_knapsack.py
+import sys
+
+sys.path.append(".")
 from src.knapsack import knapsack_01
 
-values = [60, 100, 120]
-weights = [10, 20, 30]
-capacity = 50
-max_value, selected_items = knapsack_01(values, weights, capacity)
 
-assert max_value == 220, "The max_value is wrong"
-assert selected_items == [1, 2], "The selected_items are wrong"
+def test_knapsack_01():
+    values = [60, 100, 120]
+    weights = [10, 20, 30]
+    capacity = 50
+    max_value, selected_items = knapsack_01(values, weights, capacity)
+    assert max_value == 220
+    assert selected_items == [1, 2]
